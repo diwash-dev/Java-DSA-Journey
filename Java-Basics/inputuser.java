eb1e961bd5883badb3dc846128d5288221b701ab
DIWASH <257072192+diwash-dev@users.noreply.github.com>
2026-07-27 23:46:42 +0545

    Add input user program for calculating the area of a circle and create square program

--- Java-Basics/inputuser.java
+++ Java-Basics/inputuser.java
@@ -6,9 +6,11 @@ public class inputuser {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter the radius of the circle: ");
         double r = sc.nextDouble();
         double a = 3.14 * r * r;
 
         System.out.println("Area of Circle is: " + a);
     }
 }
+
