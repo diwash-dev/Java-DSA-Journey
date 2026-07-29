954b187ec00a0a6ebf622a9dea2f8cd366b685ae
DIWASH <257072192+diwash-dev@users.noreply.github.com>
2026-07-29 23:52:57 +0545

    Learn primitive data type casting in Java

---
+++ Java-Basics/chadatatype.java
@@ -0,0 +1,12 @@
+public class chadatatype {
+    public static void main(String[] args) {
+
+        char ch = 'A';
+        System.out.println((int) ch);
+
+        int x = 5;
+        char cha = (char) x;
+        System.out.println(cha);
+    }
+
+}
