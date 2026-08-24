6af3c099f8ef065197972368bb0a7801f5af3d4f
DIWASH <257072192+diwash-dev@users.noreply.github.com>
2026-08-25 01:41:56 +0545

    absolute value program

--- Java-Basics/even_odd.java
+++ Java-Basics/even_odd.java
@@ -1,13 +1,35 @@
+// import java.util.Scanner;
+
+// public class even_odd {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter Number:");
+//         int n = sc.nextInt();
+//         if (n % 2 == 0)
+//             System.out.println("Even Number");
+//         else {
+//             System.out.println("Odd Number");
+//             System.out.println("diwash tharu ");
+//         }
+//     }
+
+// }
+
+
+
+// absolute value  examples demo 
 import java.util.Scanner;
 
 public class even_odd {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Number:");
-        int n = sc.nextInt();
-        if (n % 2 == 0)
-            System.out.println("Even Number");
+
+        Scanner sn = new Scanner(System.in);
+        System.out.println(" Enter the Number: ");
+        int n = sn.nextInt();
+        if (n >= 0)
+            System.out.println(n);
         else
-            System.out.println("Odd Number");
+            System.out.println(-n);
     }
 }
